dce397cd4acdf8d99add58473b854bdf16e84cae
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-01-28 16:59:48 -0500

    initial commit

---
+++ Homework/hw2/hw2.c
@@ -0,0 +1,22 @@
+/* Name, hw2.c, CS 24000, Spring 2020
+ * Last updated January 22, 2020
+ */
+
+/* Add any includes here */
+
+#include "hw2.h"
+
+/* Define get_average_sleep_hours here */
+
+/* Define get_sleep_log here */
+
+/* Define compare_sleep_hours here */
+
+/* Define get_average_calories here */
+
+/* Define compare_activity_log here */
+
+
+/* Remember, you don't need a main function!
+ * it is provided by hw2_main.c or hw2_test.o
+ */
