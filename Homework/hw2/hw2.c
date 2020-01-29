afbea75d51cca9156bba8f0714663480c78978f9
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-01-29 11:14:27 -0500

    Update hw2.c

--- Homework/hw2/hw2.c
+++ Homework/hw2/hw2.c
@@ -50,12 +50,16 @@ float get_average_sleep_hours(char *file_name, int year, int month) {
 
 int get_sleep_log(char *in_file, char *out_file) {
   char name[MAX_NAME_LEN];
-  int year = 2019;
-  int month = 12;
+  int year = 0;
+  int temp_year = 0;
+  int month = 0;
+  int temp_month = 0;
   int day = 0;
   float sleep_hours = 0;
   float moving_minutes = 0;
   float workout_minutes = 0;
+  float total_hours = 0;
+  float entries = 0;
   int returned_value = 1;
   FILE *input_pointer = fopen(in_file, "r");
   FILE *output_pointer = fopen(out_file, "w");
@@ -71,11 +75,22 @@ int get_sleep_log(char *in_file, char *out_file) {
     fclose(output_pointer);
     return NO_DATA_POINTS;
   }
-  else {
-    fprintf(output_pointer, "%s\n", name);
+
+  while ((returned_value = fscanf(input_pointer, "%d/%d/%d|%f|%f|%f\n",
+    &temp_month, &day, &temp_year, &sleep_hours, &moving_minutes,
+    &workout_minutes)) == 6) {
+    if (temp_year >= year) {
+      year = temp_year;
+      if (temp_month >= month) {
+        month = temp_month;
+      }
+    }
   }
 
-
+  fprintf(output_pointer, "Name: %s, Month: %d, Year: %d\n", name, month,
+    year);
+  fprintf(output_pointer, "HOUR: 0 1 2 3 4 5 6 7 8 9 10\n");
+//  fprintf(output_pointer, "%-6d|\n", day);
   return OK;
 }
 
