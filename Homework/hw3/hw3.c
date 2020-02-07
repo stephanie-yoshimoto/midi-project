7b992e4df55dbc7e7bdad0da6ad2e1f5ee26e8bf
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-02-06 21:21:49 -0500

    why won't it catch the errors right fml

--- Homework/hw3/hw3.c
+++ Homework/hw3/hw3.c
@@ -32,11 +32,12 @@ int read_tables(char *file_name) {
   int motorcycle_sales = 0;
   float commission = 0.0f;
   int returned_value = 0;
+  int read_this_iteration = 0;
   while ((returned_value = fscanf(file_ptr,
     "%39[^\n#]#%39[^\n#]#%d#%d#%d#%d#%d#%d#%d#%d#%f\n", dealership, salesperson,
     &sedan_price, &suv_price, &truck_price, &motorcycle_price, &sedan_sales,
-    &suv_sales, &truck_sales, &motorcycle_sales, &commission)) == 11) {
-    g_dealership_count++;
+    &suv_sales, &truck_sales, &motorcycle_sales, &commission) == 11)) {
+    read_this_iteration++;
     int i = 0;
     for (i = 0; i < MAX_RECORDS; i++) {
       if (g_dealerships[i][0] == '\0') {
@@ -92,16 +93,24 @@ int read_tables(char *file_name) {
     }
   }
 
+  g_dealership_count = read_this_iteration;
   fclose(file_ptr);
   file_ptr = NULL;
   if (returned_value == 12) {
     return OUT_OF_BOUNDS;
   }
-  else if (returned_value != EOF) {
+  else if ((read_this_iteration == 0) && ((returned_value == 0) ||
+    (returned_value == -1))) {
     return NO_DATA_POINTS;
   }
+  else if ((returned_value != 0) && (returned_value != -1)) {
+    return RECORD_ERROR;
+  }
+  /*else if ((read_this_iteration != 0) && ((returned_value == EOF))) {
+    return OK;
+  }*/
 
-  return OK;
+  return read_this_iteration;
 } /* read_tables() */
 
 int show_total_sales(char *out_file) {
@@ -209,13 +218,8 @@ int show_most_common_sale(char *out_file) {
     return FILE_WRITE_ERROR;
   }
 
-  int sedan = 0;
-  int suv = 1;
-  int truck = 2;
-  int motorcycle = 3;
   int vehicle_most_sold = -1;
   int largest_value = -1;
-  char *most_common_sale;
   for (int i = 0; (g_dealerships[i][0] != '\0') && (i < MAX_RECORDS); i++) {
     for (int j = 0; j < 4; j++) {
       if (g_sales[i][j] > largest_value) {
@@ -226,6 +230,7 @@ int show_most_common_sale(char *out_file) {
         vehicle_most_sold = -1;
       }
     }
+    char *most_common_sale;
     switch (vehicle_most_sold) {
       case -1: {
         most_common_sale = "More than one";
@@ -341,7 +346,15 @@ int write_tables(char *out_file, int table_index, int start_col, int end_col) {
 
 int main() {
   read_tables("no_data.txt");
-  read_tables("example.txt");
+  read_tables("test_data_files/Bogus_826");
+  read_tables("test_data_files/Empty_712");
+  read_tables("test_data_files/Input_303");
+  read_tables("test_data_files/Long_983");
   show_total_sales("output.txt");
+  show_average_prices("output.txt");
+  show_most_common_sale("output.txt");
+  write_tables("output.txt", 4, 1, 3);
+  calculate_revenue("Lafayette Motor Vehicles");
+  calculate_revenue("does not exist");
   return 0;
 }
