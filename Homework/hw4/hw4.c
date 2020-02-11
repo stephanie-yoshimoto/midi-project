3039c7d714479b488fc618db7dda760904f85965
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-02-11 11:55:21 -0500

    i actually have no idea if this will even work lol

--- Homework/hw4/hw4.c
+++ Homework/hw4/hw4.c
@@ -6,11 +6,19 @@
 
 #include <math.h>
 
+/*
+ * Adds two complex numbers' parts and returns the sum.
+ */
+
 complex_t add_complex(complex_t complex_1, complex_t complex_2) {
   complex_1.x += complex_2.x;
   complex_1.y += complex_2.y;
   return complex_1;
 } /* add_complex() */
+
+/*
+ * Negates the real and imaginary part of the complex number.
+ */
 
 complex_t neg_complex(complex_t complex_num) {
   complex_num.x *= -1;
@@ -18,13 +26,25 @@ complex_t neg_complex(complex_t complex_num) {
   return complex_num;
 } /* neg_complex() */
 
+/*
+ * Subtracts the second complex number from the first.
+ */
+
 complex_t sub_complex(complex_t complex_1, complex_t complex_2) {
   return add_complex(complex_1, neg_complex(complex_2));
 } /* sub_complex() */
 
+/*
+ * Calculates and returns the dot product of the complex arguments.
+ */
+
 double dot_complex(complex_t complex_1, complex_t complex_2) {
   return (complex_1.x * complex_2.x) + (complex_1.y * complex_2.y);
 } /* dot_complex() */
+
+/*
+ * Calculates and returns the reciprocal of the complex number.
+ */
 
 complex_t inv_complex(complex_t complex_num) {
   double magnitude = (complex_num.x * complex_num.x) + (complex_num.y *
@@ -34,6 +54,10 @@ complex_t inv_complex(complex_t complex_num) {
   return complex_num;
 } /* inv_complex() */
 
+/*
+ * Calculates and returns complex product of complex arguments.
+ */
+
 complex_t mul_complex(complex_t complex_1, complex_t complex_2) {
   double x = complex_1.x;
   complex_1.x = (complex_1.x * complex_2.x) - (complex_1.y * complex_2.y);
@@ -41,14 +65,42 @@ complex_t mul_complex(complex_t complex_1, complex_t complex_2) {
   return complex_1;
 } /* mul_complex() */
 
+/*
+ * Calculates and returns divison of first complex number by thhe second.
+ */
+
 complex_t div_complex(complex_t complex_1, complex_t complex_2) {
   return mul_complex(complex_1, inv_complex(complex_2));
 } /* div_complex() */
 
+/*
+ * Calculates and returns complex exponential function for a complex number
+ * argument using the math library functions.
+ */
+
 complex_t exp_complex(complex_t complex_num) {
+  double x = complex_num.x;
+  complex_num.x = exp(x) * cos(complex_num.y);
+  complex_num.y = exp(x) * sin(complex_num.y);
   return complex_num;
-} /* exp_complex */
+} /* exp_complex() */
+
+/*
+ * Generates a Mandelbrot set for a complex number, returns iterations
+ * necessary until magnitude is larger than 2.0.
+ */
 
 int mandelbrot(complex_t complex_num) {
-  return _COMPLEX_H;
+  int calculations = 0;
+  double dot_product = 0.0;
+  double z = 0.0;
+  while (dot_product < 4.0) {
+    z = z * z + (complex_num.x + complex_num.y);
+    dot_product = sqrt(z * z);
+    calculations++;
+    if (calculations == MAX_MANDELBROT) {
+      return MAX_MANDELBROT;
+    }
+  }
+  return calculations;
 } /* mandelbrot() */
