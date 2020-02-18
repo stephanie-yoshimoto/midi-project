488520f51cbe3c8dd5e10dda7f0b6d0d72fb2017
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-02-17 22:09:44 -0500

    first function should function

--- Homework/hw5/hw5.c
+++ Homework/hw5/hw5.c
@@ -6,26 +6,58 @@
 
 #include "hw5.h"
 
-employee_t read_employee(FILE *in_file_ptr, int employee) {
+#include <assert.h>
 
+employee_t read_employee(FILE *in_file_ptr, int employee) {
+  assert((in_file_ptr != NULL) && (employee >= 0));
+  for (int i = 0; i <= employee; i++) {
+    employee_t read_employee = {0, "", "", TECHNICIAN, 0.0, ""};
+    int returned_value = fread(&read_employee, sizeof(employee_t), 1,
+                              in_file_ptr);
+    if (returned_value != 1) {
+      return BAD_EMPLOYEE;
+    }
+
+    if (i == employee) {
+      return read_employee;
+    }
+  }
+  return BAD_EMPLOYEE;
 } /* read_employee() */
 
-int write_employee(FILE *in_file_ptr, int file_position) {
-
+int write_employee(FILE *in_file_ptr, employee_t employee, int file_position) {
+  return OK;
 } /* write_employee() */
 
-/* Define hire_employee here */
+int hire_employee(FILE *in_file_ptr, employee_t employee) {
+  return OK;
+} /* hire_employee() */
 
-/* Define fire_employee here */
+int fire_employee(FILE *in_file_ptr, employee_t employee) {
+  return OK;
+} /* fire_employee() */
 
-/* Define percent_occupancy here */
+float percent_occupancy(FILE *file_in_ptr, float salary) {
+  return 0.0;
+} /* percent_occupancy() */
 
-/* Define average_salary_by_title here */
+float average_salary_by_title(FILE *in_file_ptr, enum title_t title) {
+  return 0.0;
+} /* average_salary_by_title() */
 
-/* Define find_employee_by_id here */
+employee_t find_employee_by_id(FILE *in_file_ptr, int id_number) {
+  return BAD_EMPLOYEE;
+} /* find_employee_by_id() */
 
-/* Define find_employee_by_name here */
+employee_t find_employee_by_name(FILE *in_file_ptr, char *name,
+                                 char *last_name) {
+  return BAD_EMPLOYEE;
+} /* find_employee_by_name() */
 
-/* Define give_raise here */
+int give_raise(FILE *in_file_ptr, int id_number, float salary_increase) {
+  return OK;
+} /* give_raise() */
 
-/* Define schedule_meeting here */
+int schedule_meeting(FILE *in_file_ptr, int id_number_1, int id_number_2) {
+  return OK;
+} /* schedule_meeting() */
