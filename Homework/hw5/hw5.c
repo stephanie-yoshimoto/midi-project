a2b46db8f09237570855066d5a7288c5fb28f9fd
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-02-17 21:46:25 -0500

    initial commit

---
+++ Homework/hw5/hw5.c
@@ -0,0 +1,31 @@
+/* Stephanie Yoshimoto, hw5.c, CS 24000, Spring 2020
+ * Last updated February 3, 2020
+ */
+
+/* Add any includes here */
+
+#include "hw5.h"
+
+employee_t read_employee(FILE *in_file_ptr, int employee) {
+
+} /* read_employee() */
+
+int write_employee(FILE *in_file_ptr, int file_position) {
+
+} /* write_employee() */
+
+/* Define hire_employee here */
+
+/* Define fire_employee here */
+
+/* Define percent_occupancy here */
+
+/* Define average_salary_by_title here */
+
+/* Define find_employee_by_id here */
+
+/* Define find_employee_by_name here */
+
+/* Define give_raise here */
+
+/* Define schedule_meeting here */
