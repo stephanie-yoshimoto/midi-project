9cbfd9aaa90bc83d8d4a27540d73a0bd8dce2a4a
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-02-26 13:05:11 -0500

    initial commit

---
+++ Homework/hw6/hw6.c
@@ -0,0 +1,15 @@
+/* Name, hw6.c, CS 24000, Spring 2020
+ * Last updated February 10, 2020
+ */
+
+/* Add any includes here */
+
+#include "hw6.h"
+
+/* Define read_passwords here */
+
+/* Define locate_spies here */
+
+/* Remember, you don't need a main function!
+ * it is provided by hw6_main.c or hw6_test.o
+ */
