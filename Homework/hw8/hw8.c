fdc65af64609c9c2887b167995b5f5aeeb974457
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-03-13 11:32:58 -0700

    initial commit

---
+++ Homework/hw8/hw8.c
@@ -0,0 +1,23 @@
+/* Stephanie Yoshimoto, hw8.c, CS 24000, Spring 2020
+ * Last updated February 26, 2020
+ */
+
+#include "hw8.h"
+
+/* Define add_new_operation here */
+
+/* Define list_len here */
+
+/* Define get_nth_operation here */
+
+/* Define undo_nth_operation here */
+
+/* Define redo_n_operations here */
+
+/* Define free_list here */
+
+/* Define doc_last_line here */
+
+/* Define interleave_operations here */
+
+/* Define write_document here */
