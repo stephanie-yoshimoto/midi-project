17b2f7739ec194071889ccb15da20961c9a6dd05
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-03-28 09:50:58 -0700

    initial commit

---
+++ Project/src/library.c
@@ -0,0 +1,29 @@
+/* Name, library.c, CS 24000, Spring 2020
+ * Last updated March 27, 2020
+ */
+
+/* Add any includes here */
+
+#include "library.h"
+
+/* Define find_parent_pointer here */
+
+/* Define tree_insert here */
+
+/* Define remove_song_from_tree here */
+
+/* Define free_node here */
+
+/* Define print_node here */
+
+/* Define traverse_pre_order here */
+
+/* Define traverse_in_order here */
+
+/* Define traverse_post_order here */
+
+/* Define free_library here */
+
+/* Define write_song_list here */
+
+/* Define make_library here */
