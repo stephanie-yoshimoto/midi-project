2171fe982822f8569c88b0ca81e7ab257e216c7b
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-03-28 09:51:23 -0700

    initial commit

---
+++ Project/src/parser.c
@@ -0,0 +1,35 @@
+/* Name, parser.c, CS 24000, Spring 2020
+ * Last updated March 27, 2020
+ */
+
+/* Add any includes here */
+
+#include "parser.h"
+
+/* Define parse_file here */
+
+/* Define parse_header here */
+
+/* Define parse_track here */
+
+/* Define parse_event here */
+
+/* Define parse_sys_event here */
+
+/* Define parse_meta_event here */
+
+/* Define parse_midi_event here */
+
+/* Define parse_var_len here */
+
+/* Define event_type here */
+
+/* Define free_song here */
+
+/* Define free_track_node here */
+
+/* Define free_event_node here */
+
+/* Define end_swap_16 here */
+
+/* Define end_swap_32 here */
