b4dd3f58b551679e0dcbbe0be183cbe4e3cf1468
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-03-29 18:41:05 -0700

    help still needed

--- Project/src/library.c
+++ Project/src/library.c
@@ -1,29 +1,118 @@
-/* Name, library.c, CS 24000, Spring 2020
+/* Stephanie Yoshimoto, library.c, CS 24000, Spring 2020
  * Last updated March 27, 2020
  */
 
-/* Add any includes here */
-
 #include "library.h"
 
-/* Define find_parent_pointer here */
+#include <malloc.h>
+#include <string.h>
 
-/* Define tree_insert here */
+tree_node_t **find_parent_pointer(tree_node_t **root, const char *song_name) {
+  if (root == NULL) {
+    return NULL;
+  }
+  else if (strcmp((*root)->song_name, song_name) == 0) {
+    return root;
+  }
+  else if (strcmp((*root)->song_name, song_name) < 0) {
+    return find_parent_pointer(&((*root)->right_child), song_name);
+  }
+  else {
+    return find_parent_pointer(&((*root)->left_child), song_name);
+  }
+} /* find_parent_pointer() */
 
-/* Define remove_song_from_tree here */
+int tree_insert(tree_node_t **root, tree_node_t *insert_node) {
+  if (root == NULL) {
+    root = &insert_node;
+    return INSERT_SUCCESS;
+  }
+  else if (strcmp((*root)->song_name, insert_node->song_name) == 0) {
+    return DUPLICATE_SONG;
+  }
+  else if (strcmp((*root)->song_name, insert_node->song_name) < 0) {
+    return tree_insert(&((*root)->right_child), insert_node);
+  }
+  else {
+    return tree_insert(&((*root)->left_child), insert_node);
+  }
+} /* tree_insert() */
 
-/* Define free_node here */
+int remove_song_from_tree(tree_node_t **root, const char *song_name) {
+  tree_node_t **target = find_parent_pointer(root, song_name);
+  if (!target) {
+    return SONG_NOT_FOUND;
+  }
+  else {
+    
+    free_node(*target);
+    return DELETE_SUCCESS;
+  }
+} /* remove_song_from_tree() */
 
-/* Define print_node here */
+void free_node(tree_node_t *node) {
+  if (node->song_name) {
+    free(node->song_name);
+    node->song_name = NULL;
+  }
 
-/* Define traverse_pre_order here */
+  free_song(node->song);
+} /* free_node() */
 
-/* Define traverse_in_order here */
+void print_node(tree_node_t *node, FILE *file_ptr_out) {
+  fprintf(file_ptr_out, "%s\n", node->song_name);
+} /* print_node() */
 
-/* Define traverse_post_order here */
+void traverse_pre_order(tree_node_t *root, void *data,
+                        traversal_func_t visit) {
+  if (!root) {
+    return;
+  }
+  else {
+    (*visit)(root, data);
+    traverse_pre_order(root->left_child, data, visit);
+    traverse_pre_order(root->right_child, data, visit);
+  }
+} /* traverse_pre_order() */
 
-/* Define free_library here */
+void traverse_in_order(tree_node_t *root, void *data, traversal_func_t visit) {
+  if (!root) {
+    return;
+  }
+  else {
+    traverse_in_order(root->left_child, data, visit);
+    (*visit)(root, data);
+    traverse_in_order(root->right_child, data, visit);
+  }
+} /* traverse_in_order() */
 
-/* Define write_song_list here */
+void traverse_post_order(tree_node_t *root, void *data,
+                       traversal_func_t visit) {
+  if (!root) {
+    return;
+  }
+  else {
+    traverse_post_order(root->left_child, data, visit);
+    traverse_post_order(root->right_child, data, visit);
+    (*visit)(root, data);
+  }
+} /* traverse_post_order() */
+
+void free_library(tree_node_t *root) {
+  if (!root) {
+    return;
+  }
+  else {
+    free_library(root->left_child);
+    free_library(root->right_child);
+    free_node(root);
+  }
+} /* free_library() */
+
+void write_song_list(FILE *fp, tree_node_t *root) {
+  /*void (*func_ptr)(tree_node_t *, (void *) (FILE *)) = NULL;
+  func_ptr = print_node;*/
+  traverse_in_order(root, fp, &print_node);
+} /* write_song_list() */
 
 /* Define make_library here */
