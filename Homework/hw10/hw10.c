d49ea5956b755369ed724259a1e6facb8aed8520
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-03-30 17:06:12 -0700

    initial commit

---
+++ Homework/hw10/hw10.c
@@ -0,0 +1,21 @@
+/* Stephanie Yoshimoto, hw10.c, CS 24000, Spring 2020
+ * Last updated March 23, 2020
+ */
+
+#include "hw10.h"
+
+/* Define new_tab here */
+
+/* Define close_tab here */
+
+/* Define close_browser here */
+
+/* Define page_go_prev here */
+
+/* Define page_go_next here */
+
+/* Define open_page here */
+
+/* Define num_pages here */
+
+/* Define write_all_tabs here */
