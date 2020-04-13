d637ba2b88ea3ba59b73ea54f5a4810dffe50fa4
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-04-13 12:31:39 -0700

    initial commit

---
+++ Homework/hw12/hw12.c
@@ -0,0 +1,29 @@
+/* Stephanie Yoshimoto, hw3.c, CS 24000, Spring 2020
+ * Last updated January 29, 2020
+ */
+
+#include "hw12.h"
+
+/* Define create_cpu_data here */
+
+/* Define delete_cpu_data here */
+
+/* Define compare_cpu_data here */
+
+/* Define create_memory_data here */
+
+/* Define delete_memory_data here */
+
+/* Define compare_memory_data here */
+
+/* Define create_node here */
+
+/* Define delete_node here  */
+
+/* Define insert_node here */
+
+/* Define find_nodes here */
+
+/* Define remove_node here */
+
+/* Define delete_tree here */
