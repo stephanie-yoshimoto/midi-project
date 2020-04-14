2abb4be432153bd3ae9ca78073a8a8cc291e6114
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-04-13 19:57:17 -0700

    wow pls send help

--- Homework/hw12/hw12.c
+++ Homework/hw12/hw12.c
@@ -9,6 +9,10 @@
 #include <malloc_debug.h>
 #include <string.h>
 
+/*
+ * Creates a CPU node, sets values according to parameters.
+ */
+
 void create_cpu_data(void **ptr, const char *model, const char *manufacturer,
                      int speed, int cores) {
   assert((ptr) && (*ptr == NULL) && (model) && (manufacturer));
@@ -27,16 +31,25 @@ void create_cpu_data(void **ptr, const char *model, const char *manufacturer,
   *ptr = cpu;
 } /* create_cpu_data() */
 
-void delete_cpu_data(void **node) {
-  assert((node) && (*node));
-  cpu_t *cpu = (cpu_t *)(*node);
+/*
+ * Frees all data associated with CPU node.
+ */
+
+void delete_cpu_data(void **data) {
+  assert((data) && (*data));
+  cpu_t *cpu = (cpu_t *)(*data);
   free(cpu->model);
   cpu->model = NULL;
   free(cpu->manufacturer);
   cpu->manufacturer = NULL;
   free(cpu);
   cpu = NULL;
+  *data = NULL;
 } /* delete_cpu_data() */
+
+/*
+ * Compares overall speed of CPUs, returns values similar to those of strcmp.
+ */
 
 int compare_cpu_data(void *data_1, void *data_2) {
   assert((data_1) && (data_2));
@@ -54,6 +67,10 @@ int compare_cpu_data(void *data_1, void *data_2) {
     return 0;
   }
 } /* compare_cpu_data() */
+
+/*
+ * Creates a memory node, sets values according to parameters.
+ */
 
 void create_memory_data(void **ptr, const char *model,
                         const char *manufacturer, int size, int speed,
@@ -75,9 +92,26 @@ void create_memory_data(void **ptr, const char *model,
   *ptr = memory;
 } /* create_memory_data() */
 
-void delete_memory_data(void **ptr) {
-  assert((ptr) && (*ptr));
+/*
+ * Frees all data associated with memory node.
+ */
+
+void delete_memory_data(void **data) {
+  assert((data) && (*data));
+  memory_t *memory = (memory_t *)(*data);
+  free(memory->model);
+  memory->model = NULL;
+  free(memory->manufacturer);
+  memory->manufacturer = NULL;
+  free(memory);
+  memory = NULL;
+  *data = NULL;
 } /* delete_memory_data() */
+
+/*
+ * Compares overall memory data of node, returns values similar to those of
+ * strcmp.
+ */
 
 int compare_memory_data(void *data_1, void *data_2) {
   assert((data_1) && (data_2));
@@ -98,19 +132,14 @@ int compare_memory_data(void *data_1, void *data_2) {
   else if (manufacturer_cmp < 0) {
     return -1;
   }
-
-  int speed_1 = memory_1->size * memory_1->speed * memory_1->ddr_gen;
-  int speed_2 = memory_2->size * memory_2->speed * memory_2->ddr_gen;
-  if (speed_1 < speed_2) {
-    return -1;
-  }
-  else if (speed_1 > speed_2) {
-    return 1;
-  }
   else {
     return 0;
   }
 } /* compare_memory_data() */
+
+/*
+ * Dynamically allocates memory for new node, assigns values accordingly.
+ */
 
 void create_node(struct node **node, void *data_structure,
                  void (*print)(void *data), void (*delete)(void **node),
@@ -122,32 +151,97 @@ void create_node(struct node **node, void *data_structure,
   assert(*node);
   (*node)->left = NULL;
   (*node)->right = NULL;
+  (*node)->data = data_structure;
   (*node)->print = print;
   (*node)->delete = delete;
   (*node)->compare = compare;
 } /* create_node() */
 
-void delete_node(struct node **root) {
-  assert((root) && (*root) && ((*root)->left) && ((*root)->right));
+/*
+ * Frees all memory associated with node with callback function.
+ */
 
+void delete_node(struct node **root) {
+  assert((root) && (*root) && ((*root)->left == NULL) &&
+         ((*root)->right == NULL));
+  void (*delete)(void **root) = (*root)->delete;
+  delete(&(*root)->data);
+  free(*root);
+  *root = NULL;
 } /* delete_node() */
 
-void insert_node(struct node **root, struct node *insert_node) {
-  assert((root) && (insert_node));
+void insert_node(struct node **root, struct node *new_node) {
+  assert((root) && (new_node));
   if (!*root) {
-    *root = insert_node;
+    *root = new_node;
+    return;
+  }
+
+  int result = (*root)->compare(new_node->data, (*root)->data);
+  if (result <= 0) {
+    if (!(*root)->left) {
+      (*root)->left = new_node;
+      return;
+    }
+    insert_node(&(*root)->left, new_node);
+  }
+  else if (result > 0) {
+    if (!(*root)->right) {
+      (*root)->right = new_node;
+      return;
+    }
+    insert_node(&(*root)->right, new_node);
   }
 } /* insert_node() */
 
+void search_tree(struct node *root, void *data_structure, int *num_duplicates) {
+  if (!root) {
+    return;
+  }
+
+  int result = root->compare(data_structure, root->data);
+  if (result <= 0) {
+    if (result == 0) {
+      (*num_duplicates)++;
+    }
+
+    search_tree(root->left, data_structure, num_duplicates);
+  }
+  else if (result > 0) {
+    search_tree(root->right, data_structure, num_duplicates);
+  }
+} /* search_tree() */
+
 struct node **find_nodes(struct node *root, void *data_structure,
                          int *num_duplicates) {
-  return NULL;
+  assert((root) && (data_structure) && (num_duplicates));
+  struct node **array_of_dups = NULL;
+  search_tree(root, data_structure, num_duplicates);
+  (*num_duplicates)--;
+  array_of_dups = malloc(sizeof(struct node) * *num_duplicates);
+  assert(*array_of_dups);
+
+
+  return array_of_dups;
 } /* find_nodes() */
 
 void remove_node(struct node **root, struct node *remove_node) {
+  assert((root) && (remove_node));
 
 }
 
 void delete_tree(struct node **root) {
   assert(root);
+  if (!*root) {
+    return;
+  }
+
+  if ((*root)->left) {
+    delete_tree(&(*root)->left);
+  }
+  if ((*root)->right) {
+    delete_tree(&(*root)->right);
+  }
+  delete_node(root);
+  *root = NULL;
 } /* delete_tree() */
