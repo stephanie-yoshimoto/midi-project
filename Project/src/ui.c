46094f6314e68541a897a7b3aa894db4bfeaca28
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-04-14 22:13:35 -0700

    so unbelievably lost

--- Project/src/ui.c
+++ Project/src/ui.c
@@ -73,20 +73,43 @@ void activate(GtkApplication *app, gpointer ptr) {
   gtk_widget_set_size_request(song_list, 200, 700);
   gtk_list_box_set_selection_mode(GTK_LIST_BOX (song_list),
     GTK_SELECTION_SINGLE);
-  g_signal_connect(G_OBJECT (song_list), "selection",
-    G_CALLBACK (song_selected_cb), NULL);
   gtk_container_add(GTK_CONTAINER (window), song_list);
   gtk_grid_attach(GTK_GRID (grid), song_list, 0, 1, 2, 1);
+  /*for (int i = 0; i < 20; i++) {
+    GtkWidget *row = gtk_list_box_row_new();
+    gtk_widget_set_size_request(row, 200, 20);
+    GtkWidget *label = gtk_label_new("song name");
+    gtk_widget_set_size_request(label, 200, 15);
+    gtk_label_set_xalign(GTK_LABEL (label), 0.0);
+    gtk_container_add(GTK_CONTAINER (row), label);
+    gtk_list_box_row_set_activatable(GTK_LIST_BOX_ROW (row), true);
+    gtk_list_box_prepend(GTK_LIST_BOX (song_list), row);
+    g_signal_emit_by_name(G_OBJECT (row), "selection",
+      G_CALLBACK (song_selected_cb), NULL);
+  }*/
+
+  GtkWidget *scroll_window = gtk_scrolled_window_new(NULL, NULL);
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW (scroll_window),
+    GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+  gtk_container_add(GTK_CONTAINER (song_list), scroll_window);
 
   /* search bar */
 
-  GtkWidget *search_bar = gtk_search_entry_new();
+  GtkWidget *search_bar = gtk_search_bar_new();
   gtk_widget_set_size_request(search_bar, 200, 30);
   gtk_search_bar_set_search_mode(GTK_SEARCH_BAR (search_bar), true);
-  /*g_signal_connect_object(G_OBJECT (search_bar), "highlighted",
-    G_CALLBACK (search_bar_cb), );*/
+  g_signal_connect(search_bar, "highlighted",
+    G_CALLBACK (search_bar_cb), NULL);
   gtk_container_add(GTK_CONTAINER (window), search_bar);
   gtk_grid_attach(GTK_GRID (grid), search_bar, 0, 2, 3, 1);
+
+  /* label */
+
+  GtkWidget *song_info = gtk_label_new("Select a file from list to start...");
+  g_signal_connect(song_list, "highlighted",
+    G_CALLBACK (song_selected_cb), song_info);
+  gtk_container_add(GTK_CONTAINER (window), song_info);
+  gtk_grid_attach(GTK_GRID (grid), song_info, 2, 0, 1, 1);
 
   /* add widgets to window */
 
@@ -95,48 +118,65 @@ void activate(GtkApplication *app, gpointer ptr) {
 } /* activate() */
 
 void add_song_cb(GtkButton *button, gpointer ptr) {
-  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  /*GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   GtkWidget *dialog = gtk_file_chooser_dialog_new("Open File",
-    GTK_WINDOW (window), GTK_RESPONSE_CANCEL, "_Cancel",
-    GTK_FILE_CHOOSER_ACTION_OPEN, "_Open", NULL);
+    GTK_WINDOW (window), GTK_RESPONSE_CANCEL, "Cancel",
+    GTK_FILE_CHOOSER_ACTION_OPEN, "Open", NULL);
   gtk_container_add(GTK_CONTAINER (window), dialog);
-  gtk_widget_show_all(window);
+  gtk_widget_show_all(window);*/
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_transient_for(GTK_WINDOW (window), NULL);
+  GtkWidget *dialog = gtk_file_chooser_dialog_new("Open File",
+    GTK_WINDOW (window), GTK_RESPONSE_CANCEL, "Cancel",
+    GTK_FILE_CHOOSER_ACTION_OPEN, "Open", NULL);
 
-  if (gtk_dialog_run(GTK_DIALOG (dialog)) == GTK_FILE_CHOOSER_ACTION_OPEN) {
+  gint status = gtk_dialog_run(GTK_DIALOG (dialog));
+  if (status == GTK_FILE_CHOOSER_ACTION_OPEN) {
     gchar *file_name = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER (dialog));
     g_print("%s\n", file_name);
     /*make_library(file_name);
     update_song_list();*/
     gtk_widget_destroy(dialog);
   }
+  else if (status == GTK_RESPONSE_CANCEL) {
+    gtk_widget_destroy(dialog);
+  }
 } /* add_song_cb() */
 
 void load_songs_cb(GtkButton *button, gpointer ptr) {
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  GtkWidget *dialog = gtk_file_chooser_dialog_new("Choose Folder",
-    GTK_WINDOW (window), GTK_RESPONSE_CANCEL, "_Cancel",
-    GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER, "_Open", NULL);
+  /*GtkWidget *dialog = gtk_file_chooser_dialog_new("Choose Folder",
+    GTK_WINDOW (window), GTK_RESPONSE_CANCEL, "Cancel",
+    GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER, "Open", NULL);
   gtk_container_add(GTK_CONTAINER (window), dialog);
-  gtk_widget_show_all(window);
+  gtk_widget_show_all(window);*/
+  gtk_window_set_transient_for(GTK_WINDOW (window), NULL);
+  GtkWidget *dialog = gtk_file_chooser_dialog_new("Choose Folder",
+    GTK_WINDOW (window),
+    GTK_RESPONSE_CANCEL, "Cancel",
+    GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER, "Open", NULL);
+  gtk_widget_show(dialog);
 
-  if (gtk_dialog_run(GTK_DIALOG (dialog)) ==
-      GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER) {
+  gint status = gtk_dialog_run(GTK_DIALOG (dialog));
+  if (status == GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER) {
     gchar *dir_name =
-      gtk_file_chooser_get_current_folder(GTK_FILE_CHOOSER (dialog));
+      gtk_file_chooser_get_current_folder_uri(GTK_FILE_CHOOSER (dialog));
     g_print("%s\n", dir_name);
     /*make_library(dir_name);
     update_song_list();*/
     gtk_widget_destroy(dialog);
   }
+  else if (status == GTK_RESPONSE_CANCEL) {
+    gtk_widget_destroy(dialog);
+  }
 } /* load_songs_cb() */
 
 void song_selected_cb(GtkListBox *list_box, GtkListBoxRow *row) {
-  /*GtkListBoxRow = gtk_list_box_get_selected_row(list_box);*/
-  printf("hiii\n");
+  g_print("hellooo\n");
 } /* song_selected_cb() */
 
-void search_bar_cb(GtkSearchBar *search, gpointer ptr) {
-
+void search_bar_cb(GtkSearchBar *search_bar, gpointer ptr) {
+  g_print("hellooo\n");
 } /* search_bar_cb() */
 
 void time_scale_cb(GtkSpinButton *button, gpointer ptr) {
