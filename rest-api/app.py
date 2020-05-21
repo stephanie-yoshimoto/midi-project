4258d346bb7b959e335f200f3e72c2847763af14
Stephanie Yoshimoto <stephanie.yoshimoto@gmail.com>
2020-05-21 11:19:19 -0700

    hmm python

--- rest-api/app.py
+++ rest-api/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        result_json = request.get_json()
+        return jsonify({'sent': result_json}), 201
+    else:
+        # method is 'GET'
+        return jsonify({'about': 'Hello World'})
+
+
+@app.route('/multi/<int:num>', methods=['GET'])
+def multiply(num):
+    return jsonify({'result': num * 10})
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
 
 if __name__ == '__main__':
